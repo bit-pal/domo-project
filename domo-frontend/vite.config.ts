5144694ea36d60b92b503dca81521ed1b0e431c3
Japhet Cunanan <japscunanan21@gmail.com>
2025-06-01 23:25:04 +0800

    Fix: fix dependency issues

--- domo-frontend/vite.config.ts
+++ domo-frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,20 @@ export default defineConfig({
 	server: {
 		port: 3000,
 	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	optimizeDeps: {
+		include: [
+			'@solana/web3.js',
+			'@solana/wallet-adapter-react',
+			'@solana/wallet-adapter-base',
+			'@solana/wallet-adapter-wallets',
+			'@solana/wallet-adapter-react-ui',
+			'bs58',
+			'react-icons/fa'
+		]
+	}
 })
